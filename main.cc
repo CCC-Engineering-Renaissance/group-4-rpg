25d0e57a60c4f5e7400131b4a1f8cf804c4fcf29
julianlaxamana <37463412+julianlaxamana@users.noreply.github.com>
2024-11-04 10:47:06 -0800

    Initial commit

---
+++ main.cc
@@ -0,0 +1,19 @@
+#include "colors.h" // If you want to make your map look pretty and use colors :)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+int main(){
+  vector<vector<map>> char; // You can use a 2D vector if you'd like, but it's a lot better to emulate a 2D vector with a 1D one.
+
+  // This is where all your logic will go
+  while (true){
+    // TODO: Get user input
+
+    // TODO Move character or something cool using the user's input
+
+    // TODO: Output map
+
+  }
+}
